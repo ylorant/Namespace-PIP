3cb1c8ba33c104793cc7cf3d5f04a143a1ebf81f
linkboss <linkboss@gmail.com>
2012-03-06 00:16:59 +0100

    Updated configuration, the old one was defective.

--- application/config/config.php
+++ application/config/config.php
@@ -7,9 +7,12 @@ $config['default_controller_file'] = 'main.php'; // Default controller file to l
 $config['error_controller'] = 'Controller\\Error'; // Controller used for errors (e.g. 404, 500 etc)
 $config['error_controller_file'] = 'error.php'; // Controller file used for errors (e.g. 404, 500 etc)
 
-$config['db_host'] = ''; // Database host (e.g. localhost)
-$config['db_name'] = ''; // Database name
-$config['db_username'] = ''; // Database username
-$config['db_password'] = ''; // Database password
+define('DB_ENGINE', 'mysql'); //Database engine (actually, only mysql works)
+define('DB_HOST', 'localhost'); //Database host
+define('DB_PORT', 3306); //Database port, by default 3306
+define('DB_USER', ''); //Database user
+define('DB_PW', ''); //Database user password
+define('DB_DBNAME', ''); //Database name
+
 
 ?>
