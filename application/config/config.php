420a9c59e112eac63ce1993e9e1b62aff4d617da
linkboss <linkboss@gmail.com>
2012-07-24 01:18:25 +0200

    Re-done the page, using bootstrap.

---
+++ application/config/config.php
@@ -0,0 +1,27 @@
+<?php 
+
+$config['base_url'] = '/npip/'; // Base URL including trailing slash (e.g. http://localhost/)
+
+$config['default_controller'] = 'Controller\\Main'; // Default controller to load
+$config['default_controller_file'] = 'main.php'; // Default controller file to load
+$config['error_controller'] = 'Controller\\Error'; // Controller used for errors (e.g. 404, 500 etc)
+$config['error_controller_file'] = 'error.php'; // Controller file used for errors (e.g. 404, 500 etc)
+$config['exception_handler'] = 'exception';
+$config['debug'] = true;
+$config['hash_salt'] = ''; //Hash salt for passwords
+
+// Routes
+$config['routes'] = array(
+	new Route(Route::TYPE_STATIC, '', 'Main::index'),
+	new Route(Route::TYPE_DYNAMIC, array('{class}', '{method}', '[{string}]'), '$1::$2'),
+	new Route(Route::TYPE_DYNAMIC, array('{class}', '[{string}]'), '$1::index'),
+	new Route(Route::TYPE_DYNAMIC, array('{m:Main}', '[{string}]'), 'Main::$1')
+);
+
+define('DB_ENGINE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_USER', '');
+define('DB_PW', '');
+define('DB_DBNAME', '');
+?>
