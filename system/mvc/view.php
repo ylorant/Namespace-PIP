d7bba1b848589a2b07f14712001b9ec583bb9c19
Yohann Lorant <yohann.lorant@gmail.com>
2015-06-04 01:07:14 +0200

    Fixed HTTPS urls on view URL rewriting.

--- system/mvc/view.php
+++ system/mvc/view.php
@@ -77,9 +77,9 @@ class View {
 		$content = ob_get_clean();
 		$content = preg_replace('#<img( .+)? src="(.+)"( .+)? />#isU', '<img$1 src="'.BASE_URL.'$2"$3 />', $content);
 		$content = preg_replace('#<script( .+)? src="(.+)"( .+)?>#isU', '<script$1 src="'.BASE_URL.'$2"$3>', $content);
-		$content = preg_replace('#<script( .+)? src="'.BASE_URL.'http://(.+)"( .+)?>#isU', '<script$1 src="http://$2"$3>', $content);
+		$content = preg_replace('#<script( .+)? src="'.BASE_URL.'http(s)?://(.+)"( .+)?>#isU', '<script$1 src="http$2://$3"$4>', $content);
 		$content = preg_replace('#<a( .+)? href="(.+)"( .+)?>#isU', '<a$1 href="'.BASE_URL.'$2"$3>', $content);
-		$content = preg_replace('#<a( .+)? href="'.BASE_URL.'http://(.+)"( .+)?>#isU', '<a$1 href="http://$2"$3>', $content);
+		$content = preg_replace('#<a( .+)? href="'.BASE_URL.'http(s)?://(.+)"( .+)?>#isU', '<a$1 href="http$2://$3"$4>', $content);
 		$content = preg_replace('#<link( .+)? href="(.+)"( .+)?>#isU', '<link$1 href="'.BASE_URL.'$2"$3>', $content);
 		$content = preg_replace('#<form( .+)? action="(.+)"( .+)?>#isU', '<form$1 action="'.BASE_URL.'$2"$3>', $content);
 		
